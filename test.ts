b69a3b37ede9678d879f31a6bfc73c8ce4cd2c0b
Thais Marcon <tmarcon@z2r6p2.le-101.fr>
2020-02-27 17:13:21 +0100

    rob1

--- test.ts
+++ test.ts
@@ -18,7 +18,7 @@ function init_box()
   let id2 = Math.floor(Math.random() * 16 + 1);
   while (id1 == id2)
   {
-    id2 = Math.floor(Math.random() * 15 + 1)
+    id2 = Math.floor(Math.random() * 16 + 1)
   }
   console.log(id1)
   console.log(id2)
@@ -26,6 +26,7 @@ function init_box()
   let sb = document.createElement("div")
   let p = document.createElement("p")
   sb.classList.add("s2")
+  sb.classList.add("sb")
   let newContent = document.createTextNode((2).toString())
   p.appendChild(newContent)
   sb.appendChild(p)
@@ -37,6 +38,7 @@ function init_box()
   newContent = document.createTextNode((nbr_2_4()).toString())
   sb.appendChild(newContent)
   sb.classList.add("s"+sb.innerHTML)
+  sb.classList.add("sb")
   p.appendChild(newContent)
   sb.appendChild(p)
   box.appendChild(sb)
@@ -46,14 +48,56 @@ function nbr_2_4()
 {
   let nbr_rand = Math.random()
   if (nbr_rand > 0.9)
-  {
     return 4;
-  }
   else
-  {
     return 2;
-  }
 }
 
-create_box();
+create_box()
 init_box()
+
+function check_exist(id) {
+  elems = document.getElementsByClassName("sb");
+  for (i = 0; i < elems.length; i++)
+    if ("case_" + id == elems[i].parentElement.id)
+      return (1);
+  return (0);
+
+}
+
+function add_box()
+{
+  let id1 = Math.floor(Math.random() * 16 + 1);
+  let id2 = Math.floor(Math.random() * 16 + 1);
+  while (check_exist(id1))
+  {
+    id1 = Math.floor(Math.random() * 16 + 1)
+      if (document.getElementsByClassName("sb").length == 16)
+        return
+  }
+  let box = document.getElementById("case_" + id1.toString());
+  let sb = document.createElement("div")
+  let p = document.createElement("p")
+  let newContent = document.createTextNode((nbr_2_4()).toString())
+  p.appendChild(newContent)
+  sb.classList.add("s"+p.innerHTML)
+  sb.classList.add("sb")
+  sb.appendChild(p)
+  box.appendChild(sb)
+}
+
+function ft_push_right()
+{
+  add_box();
+}
+
+$(document).on("keydown", function(e){
+  if (e.key == "ArrowRight")
+    ft_push_right()
+  if (e.key == "ArrowLeft")
+    ft_push_right()
+  if (e.key == "ArrowUp")
+    ft_push_right()
+  if (e.key == "ArrowDown")
+    ft_push_right()
+});
