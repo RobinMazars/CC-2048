153a0cc7213ed37e6c351103488c911d8e042c1b
Robin Mazars <rmazars@z2r6p4.le-101.fr>
2020-02-27 14:33:42 +0100

    random 2 on start

--- test.ts
+++ test.ts
@@ -7,13 +7,27 @@ function create_box()
     let box = document.createElement("div")
     box.classList.add("box")
     box.setAttribute("id", "case_"+(j+1).toString())
-    var newContent = document.createTextNode((j+1).toString())
-    box.appendChild(newContent)
     frame.appendChild(box)
     j++
   }
   console.log("END");
 }
+function init_box()
+{
+  let i:number = 0;
+  while (i < 2)
+  {
+    let id = Math.floor(Math.random() * 16);
+    let box = document.getElementById("case_"+id.toString());
+    let sb = document.createElement("div")
+    sb.classList.add("s2")
+    let newContent = document.createTextNode((2).toString())
+    sb.appendChild(newContent)
+    box.appendChild(sb)
+    i++;
+  }
 
+}
 
 create_box();
+init_box()
