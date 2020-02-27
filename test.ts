ad72ab7f3d07ef30a4acafde391da41019ce21c5
Robin Mazars <rmazars@z2r6p4.le-101.fr>
2020-02-27 14:16:38 +0100

    add js for create box

---
+++ test.ts
@@ -0,0 +1,19 @@
+function create_box()
+{
+  let frame = document.getElementById("frame");
+  let j = 0;
+  while(j < 16)
+  {
+    let box = document.createElement("div")
+    box.classList.add("box")
+    box.setAttribute("id", "case_"+(j+1).toString())
+    var newContent = document.createTextNode((j+1).toString())
+    box.appendChild(newContent)
+    frame.appendChild(box)
+    j++
+  }
+  console.log("END");
+}
+
+
+create_box();
