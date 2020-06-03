6c2860bd9ecc60719dde96eafdd48078f752fa25
KingDCreations <kingdiamsxd@gmail.com>
2020-06-03 15:15:35 +0200

    Adding animations

--- public/js/access.js
+++ public/js/access.js
@@ -4,3 +4,25 @@ window.addEventListener("keydown", function(e) {
     return;
   }
 });
+
+o = true;
+function reset(){
+  arr = document.getElementsByClassName("sb");
+
+  if (o == true) {
+    for (const e of arr) {
+      e.style.width = "0%";
+      e.style.height = "0%";
+      e.style.opacity = 0;
+    }
+    o = false;
+  }
+  else {
+    for (const e of arr) {
+      e.style.width = "100%";
+      e.style.height = "100%";
+      e.style.opacity = 1;
+    }
+    o = true;
+  }
+}
